ed8b20321472e934b5ec477c632ed7b2b4e6daaa
vrobert <vincent.robert@nanego.com>
2016-07-07 14:37:46 +0200

    Update plugin URL in init.rb

--- init.rb
+++ init.rb
@@ -5,9 +5,9 @@ require 'redmine_fix/fix_redmine_headers_middleware'
 Redmine::Plugin.register :redmine_fix do
   name 'Redmine Fix Headers Middleware plugin'
   description 'This plugin is a private, specific plugin for my work'
-  url 'https://github.com/nanego'
+  url 'https://github.com/nanego/redmine_fix_headers_middleware'
   author 'Vincent ROBERT'
   author_url 'mailto:contact@vincent-robert.com'
   requires_redmine :version_or_higher => '2.5.0'
-  version '0.1'
+  version '1.0'
 end
